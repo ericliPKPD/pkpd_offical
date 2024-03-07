50be4540cbab437337a14e16a55ca12a44297abf
ericliPKPD <www.liwaileong@gmail.com>
2024-03-07 12:09:56 +0800

    testing

--- app/src/main/java/com/example/myapplication_1/information_setting.java
+++ app/src/main/java/com/example/myapplication_1/information_setting.java
@@ -1,22 +1,19 @@
 package com.example.myapplication_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 
-import com.google.android.material.switchmaterial.SwitchMaterial;
-
-public class information extends AppCompatActivity {
+public class information_setting extends AppCompatActivity {
 
     Button back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.information_setting);
+        setContentView(R.layout.information);
 
         // initialize variable (backward button)
         back_btn = (Button) findViewById(R.id.cancel_button);
@@ -25,7 +22,7 @@ public class information extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // jump from setting page to home page
-                Intent settings = new Intent(information.this, setting_activity.class);
+                Intent settings = new Intent(information_setting.this, setting_activity.class);
                 startActivity(settings);
             }
         });
