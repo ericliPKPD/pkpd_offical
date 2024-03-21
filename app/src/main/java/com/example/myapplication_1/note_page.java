eac2cf562234c8aa30d709473dc92de4ba5e45b5
Joeling1 <162530214+Joeling1@users.noreply.github.com>
2024-03-21 10:37:36 +0800

    Update note_page.java

--- app/src/main/java/com/example/myapplication_1/note_page.java
+++ app/src/main/java/com/example/myapplication_1/note_page.java
@@ -1,1 +1,15 @@
+package com.example.myapplication_1;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
